e97d688e18ece9f493b6a7b71d2e672cd356798e
William Elliott <wcelliott1991@gmail.com>
2017-09-25 22:28:32 -0400

    initializes react-redux blog app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src='https://cdn4.iconfinder.com/data/icons/free-3d-social-icons/png/512x512/Online%20writing.png' className="App-logo" alt="logo" />
+          <h2>Blog App</h2>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
