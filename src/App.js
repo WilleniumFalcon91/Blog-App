7d50b7fbf8486ffd21432465e0750932c7e79d31
William Elliott <wcelliott1991@gmail.com>
2017-11-06 17:03:28 -0500

    initial setup of routers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render() { return <div>Hello!</div>}
+}
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div>}
+}
 
 class App extends Component {
   render() {
@@ -10,7 +23,14 @@ class App extends Component {
           <img src='https://cdn4.iconfinder.com/data/icons/free-3d-social-icons/png/512x512/Online%20writing.png' className="App-logo" alt="logo" />
           <h2>Blog App</h2>
         </div>
-
+        <Provider store={createStoreWithMiddleware}>
+          <BrowserRouter>
+              <div>
+                  <Route path="/hello" component={Hello}/>
+                  <Route path="/goodbye" component={Goodbye} />
+              </div>
+          </BrowserRouter>
+        </Provider>
       </div>
     );
   }
