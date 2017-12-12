5dce5feb7eb1c6aadbed2b4de35c1ed75fe04cf0
William Elliott <wcelliott1991@gmail.com>
2017-12-12 11:34:13 -0500

    creates action

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import promise from 'redux-promise';
 
 // import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <div className="App">
