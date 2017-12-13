ed992fbe67c10e100d24a130093e3dba25b103c1
William Elliott <wcelliott1991@gmail.com>
2017-12-13 09:11:12 -0500

    imports Switch component from react-router-dom to list most specific routes first and then "switch" to the default instead of rendering boths component routes on the same page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import promise from 'redux-promise';
 import { rootReducer } from './reducers/index';
 // import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -21,13 +22,16 @@ ReactDOM.render(
     <div className="App">
         <div className="App-header">
             <img src='https://cdn4.iconfinder.com/data/icons/free-3d-social-icons/png/512x512/Online%20writing.png' className="App-logo" alt="logo" />
-            <h2>Blog App</h2>
+            <h2 className="blog-name">Blog App</h2>
         </div>
     
         <Provider store={configureStore()}>
             <BrowserRouter>
                 <div>
-                    <Route path='/' component={PostsIndex} />
+                    <Switch>
+                        <Route path='/posts/new' component={PostsNew} />
+                        <Route path='/' component={PostsIndex} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         </Provider>
