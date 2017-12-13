34a4d657f5fa088ea9510278ed3b12125a6a9b23
William Elliott <wcelliott1991@gmail.com>
2017-12-13 11:24:10 -0500

    creates initial new post component

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <div>
+                Write a new post!
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
