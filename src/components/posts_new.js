0983efa4f897a3974a98dc793fc59604a1841f5c
William Elliott <wcelliott1991@gmail.com>
2017-12-13 21:24:01 -0500

    routes post data to the api onSubmit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -21,7 +24,7 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values);
     }
 
     render() {
@@ -47,9 +50,7 @@ class PostsNew extends Component {
             <button type="submit" className="btn btn-primary submit">
                 Submit
             </button>
-            <button type="cancel" className="btn btn-danger">
-                Cancel
-            </button>
+            <Link className="btn btn-danger" to="/">Cancel</Link>
             </form>
         );
     }
@@ -72,4 +73,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
